186e905f1b3b48c52086410a09312111300dedc4
Emory Soper <emorysoper@icloud.com>
2023-04-12 10:42:53 -0400

    made functions

--- js/site.js
+++ js/site.js
@@ -1,11 +1,42 @@
-function displayMessage() {
-    let msg = document.getElementById("message").value;
-    //alert(msg);
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'APP NAME',
-            text: msg
+function fizzBuzz() {
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    let table = document.getElementById("results");
+
+    if (startValue == "" || endValue == "") {
+        table.innerHTML = "<tr><td>Error: Missing Value</td></tr>";
+        return;
+    }
+
+    startNumber = parseInt(startValue);
+    endNumber = parseInt(endValue);
+
+    table.innerHTML = "";
+    
+    if (startNumber <= endNumber) {
+        for (i = startNumber; i <= endNumber; i++) {
+            table.innerHTML += "<tr><td>" + fizzCheck(i) + "</td></tr>";
         }
-    );
+    } else {
+        for (i = startNumber; i >= endNumber; i--) {
+            table.innerHTML += "<tr><td>" + fizzCheck(i) + "</td></tr>";
+        }
+    }
+}
+
+function fizzCheck(num){
+    var unfizzed = true;
+    var out = "";
+    if(num%3 == 0){
+        out += "Fizz";
+        unfizzed = false;
+    }
+    if(num%5 == 0){
+        out += "Buzz";
+        unfizzed = false;
+    }
+    if(unfizzed){
+        out += num;
+    }
+    return out;
 }
